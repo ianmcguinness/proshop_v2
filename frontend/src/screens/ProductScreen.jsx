48f39d5c82d7145acc2f67165125351f87810601
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-17 01:57:50 +0000

    Fetch products from backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,19 @@
+import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = () => {
-  const { id: productId } = useParams()
-  const product = products.find(p => p._id === productId)
+  const { id } = useParams()
+  const [product, setProduct] = useState({})
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [id])
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
