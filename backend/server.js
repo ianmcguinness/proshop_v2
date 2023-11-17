8036b90d96003309c8543236361d15a4f5118bb4
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-17 13:25:53 +0000

    Products from backend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,13 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
 
 const app = express()
 dotenv.config()
 connectDB()
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const { id } = req.params
-  const product = products.find(p => p._id === id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 const port = process.env.PORT || 5000
 app.listen(port, () =>
