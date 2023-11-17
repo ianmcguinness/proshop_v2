819cd4bed44aa2d5c6d005072a0c9b5372fa3dac
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-17 11:45:13 +0000

    Models setup

---
+++ backend/models/reviewModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+export const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    rating: {
+      type: Number,
+      required: true
+    },
+    comment: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
