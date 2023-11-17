b236e147687157e7639768ad4340c9e2c6d7555a
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-17 14:55:44 +0000

    Redux toolkit setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,13 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
+import { Provider } from 'react-redux'
+import store from './store.js'
 import App from './App.jsx'
 import HomeScreen from './screens/HomeScreen.jsx'
 import ProductScreen from './screens/ProductScreen.jsx'
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +25,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
