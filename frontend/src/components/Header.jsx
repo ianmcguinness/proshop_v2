9fa9a66ad6d979c0039242632443905aa876b504
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-18 11:49:59 +0000

    Logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,26 @@
-import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import { Navbar, Nav, NavDropdown, Container, Badge } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import { useLogoutMutation } from '../slices/usersApiSlice'
+import { logout } from '../slices/authSlice'
 import Logo from '../assets/logo.png'
 
 const Header = () => {
   const { cartItems } = useSelector(state => state.cart)
   const { userInfo } = useSelector(state => state.auth)
-  const logoutHandler = () => {
-    console.log('Logout')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [logoutApiCall] = useLogoutMutation()
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap()
+      dispatch(logout())
+      navigate('/login')
+    } catch (err) {
+      console.error(err)
+    }
   }
   return (
     <header>
