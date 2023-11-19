b4ef330ecba1d3b649ab3a7e89f4a314bf58a010
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-19 15:59:51 +0000

    Shipping screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import ProductScreen from './screens/ProductScreen.jsx'
 import CartScreen from './screens/CartScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
+import ShippingScreen from './screens/ShippingScreen.jsx'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 
@@ -25,6 +26,7 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      <Route path='/shipping' element={<ShippingScreen />} />
     </Route>
   )
 )
