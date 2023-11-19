1baf4a947ce52e5d4665c0a80c4ebddbb2d73d50
Ian McGuinness <ian.mcguinness@outlook.com>
2023-11-19 16:13:01 +0000

    Private Route component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,18 +15,22 @@ import CartScreen from './screens/CartScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
 import ShippingScreen from './screens/ShippingScreen.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
+      <Route path='' element={<PrivateRoute />}>
+        {/* Private Routes go here */}
+        <Route path='/shipping' element={<ShippingScreen />} />
+      </Route>
       <Route index path='/' element={<HomeScreen />} />
       <Route path='/products/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
-      <Route path='/shipping' element={<ShippingScreen />} />
     </Route>
   )
 )
